d32218c8b5444a992ddadf067b228a6c1c035698
jadasmochidonuts <jadasmochidonuts@gmail.com>
2026-05-31 14:46:11 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import JadasMochiDonuts from '../jadas-mochi-donuts.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JadasMochiDonuts />
+  </React.StrictMode>
+)
